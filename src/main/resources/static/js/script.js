57b59b25d44806367a933c980bc9215bc2cf001a
hiterharris <HiterHarrisIV@Hs-MacBook-Pro.local>
2017-05-01 11:09:40 -0400

    day selector on click js

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,89 @@
+// ------ Daily Meeting Selector --------
+document.getElementById("su").onclick = function() {
+  $('#sunday').show();
+    $('#monday').hide();
+    $('#tuesday').hide();
+    $('#wednesday').hide();
+    $('#thursday').hide();
+    $('#friday').hide();
+    $('#saturday').hide();
+    $('#top-five').hide();
+};
+
+
+document.getElementById("m").onclick = function() {
+  $('#monday').show();
+    $('#sunday').hide();
+    $('#tuesday').hide();
+    $('#wednesday').hide();
+    $('#thursday').hide();
+    $('#friday').hide();
+    $('#saturday').hide();
+    $('#top-five').hide();
+};
+
+document.getElementById("tu").onclick = function() {
+  $('#tuesday').show();
+    $('#sunday').hide();
+    $('#monday').hide();
+    $('#wednesday').hide();
+    $('#thursday').hide();
+    $('#friday').hide();
+    $('#saturday').hide();
+    $('#top-five').hide();
+};
+
+document.getElementById("w").onclick = function() {
+  $('#wednesday').show();
+    $('#sunday').hide();
+    $('#monday').hide();
+    $('#tuesday').hide();
+    $('#thursday').hide();
+    $('#friday').hide();
+    $('#saturday').hide();
+    $('#top-five').hide();
+};
+
+document.getElementById("th").onclick = function() {
+  $('#thursday').show();
+    $('#sunday').hide();
+    $('#monday').hide();
+    $('#tuesday').hide();
+    $('#wednesday').hide();
+    $('#friday').hide();
+    $('#saturday').hide();
+    $('#top-five').hide();
+};
+
+document.getElementById("f").onclick = function() {
+  $('#friday').show();
+    $('#sunday').hide();
+    $('#monday').hide();
+    $('#tuesday').hide();
+    $('#wednesday').hide();
+    $('#thursday').hide();
+    $('#saturday').hide();
+    $('#top-five').hide();
+};
+
+document.getElementById("sa").onclick = function() {
+  $('#saturday').show();
+    $('#sunday').hide();
+    $('#monday').hide();
+    $('#tuesday').hide();
+    $('#wednesday').hide();
+    $('#thursday').hide();
+    $('#friday').hide();
+    $('#top-five').hide();
+};
+
+document.getElementById("quick-btn").onclick = function() {
+  $('#top-five').show();
+    $('#sunday').hide();
+    $('#monday').hide();
+    $('#tuesday').hide();
+    $('#wednesday').hide();
+    $('#thursday').hide();
+    $('#friday').hide();
+    $('#saturday').hide();
+};
