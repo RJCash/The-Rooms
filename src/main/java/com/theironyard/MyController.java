4b7fef642760537d4b29e942445e91da47f63c19
Rickie Cashwell <rjc6056@gmail.com>
2017-05-02 09:36:23 -0400

    database list method

--- src/main/java/com/theironyard/MyController.java
+++ src/main/java/com/theironyard/MyController.java
@@ -1,18 +1,23 @@
 package com.theironyard;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by rickiecashwell on 4/28/17.
  */
 @Controller
 public class MyController {
+@Autowired
+GroupRepository repo;
 
     @RequestMapping("/")
-    public String Home(){
+    public String Home(Model model, @RequestParam(defaultValue = "Monday") String day){
 
-
+        model.addAttribute("groups",repo.listGroups(day));
         return "index";
     }
 
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,111 @@
+// ------ Daily Meeting Selector --------
+document.getElementById("su").onclick = function() {
+  $('#sunday').show();
+    $('#monday').hide();
+    $('#tuesday').hide();
+    $('#wednesday').hide();
+    $('#thursday').hide();
+    $('#friday').hide();
+    $('#saturday').hide();
+    $('#top-five').hide();
+};
+
+
+document.getElementById("m").onclick = function() {
+  $('#monday').show();
+    $('#sunday').hide();
+    $('#tuesday').hide();
+    $('#wednesday').hide();
+    $('#thursday').hide();
+    $('#friday').hide();
+    $('#saturday').hide();
+    $('#top-five').hide();
+};
+
+document.getElementById("tu").onclick = function() {
+  $('#tuesday').show();
+    $('#sunday').hide();
+    $('#monday').hide();
+    $('#wednesday').hide();
+    $('#thursday').hide();
+    $('#friday').hide();
+    $('#saturday').hide();
+    $('#top-five').hide();
+};
+
+document.getElementById("w").onclick = function() {
+  $('#wednesday').show();
+    $('#sunday').hide();
+    $('#monday').hide();
+    $('#tuesday').hide();
+    $('#thursday').hide();
+    $('#friday').hide();
+    $('#saturday').hide();
+    $('#top-five').hide();
+};
+
+document.getElementById("th").onclick = function() {
+  $('#thursday').show();
+    $('#sunday').hide();
+    $('#monday').hide();
+    $('#tuesday').hide();
+    $('#wednesday').hide();
+    $('#friday').hide();
+    $('#saturday').hide();
+    $('#top-five').hide();
+};
+
+document.getElementById("f").onclick = function() {
+  $('#friday').show();
+    $('#sunday').hide();
+    $('#monday').hide();
+    $('#tuesday').hide();
+    $('#wednesday').hide();
+    $('#thursday').hide();
+    $('#saturday').hide();
+    $('#top-five').hide();
+};
+
+document.getElementById("sa").onclick = function() {
+  $('#saturday').show();
+    $('#sunday').hide();
+    $('#monday').hide();
+    $('#tuesday').hide();
+    $('#wednesday').hide();
+    $('#thursday').hide();
+    $('#friday').hide();
+    $('#top-five').hide();
+};
+
+// -------- Quick Find Button --------
+
+document.getElementById("quick-btn").onclick = function() {
+  document.querySelector('#quick-btn').style.visibility = "hidden";
+  document.querySelector('#selector').style.visibility = "hidden";
+  document.querySelector('#back').style.visibility = "visible";
+  document.querySelector('#quick-info').style.visibility = "visible";
+  $('#top-five').show();
+    $('#sunday').hide();
+    $('#monday').hide();
+    $('#tuesday').hide();
+    $('#wednesday').hide();
+    $('#thursday').hide();
+    $('#friday').hide();
+    $('#saturday').hide();
+
+};
+
+document.getElementById("back").onclick = function() {
+  document.querySelector('#quick-btn').style.visibility = "visible";
+  document.querySelector('#selector').style.visibility = "visible";
+  document.querySelector('#back').style.visibility = "hidden";
+  document.querySelector('#quick-info').style.visibility = "hidden";
+  $('#sunday').show();
+  $('#monday').show();
+  $('#tuesday').show();
+  $('#wednesday').show();
+  $('#thursday').show();
+  $('#friday').show();
+  $('#saturday').show();
+  $('#top-five').show();
+};
