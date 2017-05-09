5710ababde7db43c3f21dd2e774c31dc41e69d1e
hiterharris <HiterHarrisIV@Hs-MacBook-Pro.local>
2017-05-09 14:05:13 -0400

    added javascript for selector color changer...and some css

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -13,6 +13,172 @@ jQuery(document).ready(function($) {
 
 
 
+// document.getElementById("su").onclick = function() {
+//   document.querySelector('#su').style.color = "#276AAD";
+//   document.querySelector('#m').style.color = "black";
+//   document.querySelector('#tu').style.color = "black";
+//   document.querySelector('#w').style.color = "black";
+//   document.querySelector('#th').style.color = "black";
+//   document.querySelector('#f').style.color = "black";
+//   document.querySelector('#sa').style.color = "black";
+//   $('#sunday').show();
+//
+//     $('#monday').hide();
+//     $('#tuesday').hide();
+//     $('#wednesday').hide();
+//     $('#thursday').hide();
+//     $('#friday').hide();
+//     $('#saturday').hide();
+//     $('#top-five').hide();
+// };
+// document.getElementById("m").onclick = function() {
+//   document.querySelector('#m').style.color = "#276AAD";
+//   document.querySelector('#su').style.color = "black";
+//   document.querySelector('#tu').style.color = "black";
+//   document.querySelector('#w').style.color = "black";
+//   document.querySelector('#th').style.color = "black";
+//   document.querySelector('#f').style.color = "black";
+//   document.querySelector('#sa').style.color = "black";
+//   $('#monday').show();
+//     $('#sunday').hide();
+//     $('#tuesday').hide();
+//     $('#wednesday').hide();
+//     $('#thursday').hide();
+//     $('#friday').hide();
+//     $('#saturday').hide();
+//     $('#top-five').hide();
+// };
+//
+// document.getElementById("tu").onclick = function() {
+//   document.querySelector('#tu').style.color = "#276AAD";
+//   document.querySelector('#su').style.color = "black";
+//   document.querySelector('#m').style.color = "black";
+//   document.querySelector('#w').style.color = "black";
+//   document.querySelector('#th').style.color = "black";
+//   document.querySelector('#f').style.color = "black";
+//   document.querySelector('#sa').style.color = "black";
+//   $('#tuesday').show();
+//     $('#sunday').hide();
+//     $('#monday').hide();
+//     $('#wednesday').hide();
+//     $('#thursday').hide();
+//     $('#friday').hide();
+//     $('#saturday').hide();
+//     $('#top-five').hide();
+// };
+//
+// document.getElementById("w").onclick = function() {
+//   document.querySelector('#w').style.color = "#276AAD";
+//   document.querySelector('#su').style.color = "black";
+//   document.querySelector('#tu').style.color = "black";
+//   document.querySelector('#m').style.color = "black";
+//   document.querySelector('#th').style.color = "black";
+//   document.querySelector('#f').style.color = "black";
+//   document.querySelector('#sa').style.color = "black";
+//   $('#wednesday').show();
+//     $('#sunday').hide();
+//     $('#monday').hide();
+//     $('#tuesday').hide();
+//     $('#thursday').hide();
+//     $('#friday').hide();
+//     $('#saturday').hide();
+//     $('#top-five').hide();
+// };
+//
+// document.getElementById("th").onclick = function() {
+//   document.querySelector('#th').style.color = "#276AAD";
+//   document.querySelector('#su').style.color = "black";
+//   document.querySelector('#tu').style.color = "black";
+//   document.querySelector('#w').style.color = "black";
+//   document.querySelector('#m').style.color = "black";
+//   document.querySelector('#f').style.color = "black";
+//   document.querySelector('#sa').style.color = "black";
+//   $('#thursday').show();
+//     $('#sunday').hide();
+//     $('#monday').hide();
+//     $('#tuesday').hide();
+//     $('#wednesday').hide();
+//     $('#friday').hide();
+//     $('#saturday').hide();
+//     $('#top-five').hide();
+// };
+//
+// document.getElementById("f").onclick = function() {
+//   document.querySelector('#f').style.color = "#276AAD";
+//   document.querySelector('#su').style.color = "black";
+//   document.querySelector('#tu').style.color = "black";
+//   document.querySelector('#w').style.color = "black";
+//   document.querySelector('#th').style.color = "black";
+//   document.querySelector('#m').style.color = "black";
+//   document.querySelector('#sa').style.color = "black";
+//   $('#friday').show();
+//     $('#sunday').hide();
+//     $('#monday').hide();
+//     $('#tuesday').hide();
+//     $('#wednesday').hide();
+//     $('#thursday').hide();
+//     $('#saturday').hide();
+//     $('#top-five').hide();
+// };
+//
+// document.getElementById("sa").onclick = function() {
+//   document.querySelector('#sa').style.color = "#276AAD";
+//   document.querySelector('#su').style.color = "black";
+//   document.querySelector('#tu').style.color = "black";
+//   document.querySelector('#w').style.color = "black";
+//   document.querySelector('#th').style.color = "black";
+//   document.querySelector('#f').style.color = "black";
+//   document.querySelector('#m').style.color = "black";
+//   $('#saturday').show();
+//     $('#sunday').hide();
+//     $('#monday').hide();
+//     $('#tuesday').hide();
+//     $('#wednesday').hide();
+//     $('#thursday').hide();
+//     $('#friday').hide();
+//     $('#top-five').hide();
+// };
+//
+// // -------- Quick Find Button --------
+//
+// document.getElementById("quick-btn").onclick = function() {
+//   document.querySelector('#quick-btn').style.visibility = "hidden";
+//   document.querySelector('#selector').style.visibility = "hidden";
+//   document.querySelector('#quick-btn').style.display = "none";
+//   document.querySelector('#selector').style.display = "none";
+//   document.querySelector('#back').style.visibility = "visible";
+//   document.querySelector('#quick-info').style.visibility = "visible";
+//   $('#top-five').show();
+//     $('#sunday').hide();
+//     $('#monday').hide();
+//     $('#tuesday').hide();
+//     $('#wednesday').hide();
+//     $('#thursday').hide();
+//     $('#friday').hide();
+//     $('#saturday').hide();
+//
+// };
+//
+//   document.getElementById("back").onclick = function() {
+//
+//   document.querySelector('#quick-btn').style.visibility = "visible";
+//   document.querySelector('#selector').style.visibility = "visible";
+//
+//   document.querySelector('#quick-btn').style.display = "flex";
+//   document.querySelector('#selector').style.display = "flex";
+//   document.querySelector('#back').style.visibility = "hidden";
+//   document.querySelector('#quick-info').style.visibility = "hidden";
+//   $('#sunday').show();
+//   $('#monday').show();
+//   $('#tuesday').show();
+//   $('#wednesday').show();
+//   $('#thursday').show();
+//   $('#friday').show();
+//   $('#saturday').show();
+//   $('#top-five').show();
+// };
+
+// ------Selector Color 2 ------------
 document.getElementById("su").onclick = function() {
   document.querySelector('#su').style.color = "#276AAD";
   document.querySelector('#m').style.color = "black";
@@ -21,15 +187,14 @@ document.getElementById("su").onclick = function() {
   document.querySelector('#th').style.color = "black";
   document.querySelector('#f').style.color = "black";
   document.querySelector('#sa').style.color = "black";
-  $('#sunday').show();
+  document.querySelector('#su').style.borderBottom = "3px solid #276AAD";
+  document.querySelector('#m').style.borderBottom = "3px solid transparent";
+  document.querySelector('#tu').style.borderBottom = "3px solid transparent";
+  document.querySelector('#w').style.borderBottom = "3px solid transparent";
+  document.querySelector('#th').style.borderBottom = "3px solid transparent";
+  document.querySelector('#f').style.borderBottom = "3px solid transparent";
+  document.querySelector('#sa').style.borderBottom = "3px solid transparent";
 
-    $('#monday').hide();
-    $('#tuesday').hide();
-    $('#wednesday').hide();
-    $('#thursday').hide();
-    $('#friday').hide();
-    $('#saturday').hide();
-    $('#top-five').hide();
 };
 document.getElementById("m").onclick = function() {
   document.querySelector('#m').style.color = "#276AAD";
@@ -39,16 +204,14 @@ document.getElementById("m").onclick = function() {
   document.querySelector('#th').style.color = "black";
   document.querySelector('#f').style.color = "black";
   document.querySelector('#sa').style.color = "black";
-  $('#monday').show();
-    $('#sunday').hide();
-    $('#tuesday').hide();
-    $('#wednesday').hide();
-    $('#thursday').hide();
-    $('#friday').hide();
-    $('#saturday').hide();
-    $('#top-five').hide();
+  document.querySelector('#m').style.borderBottom = "3px solid #276AAD";
+  document.querySelector('#su').style.borderBottom = "3px solid transparent";
+  document.querySelector('#tu').style.borderBottom = "3px solid transparent";
+  document.querySelector('#w').style.borderBottom = "3px solid transparent";
+  document.querySelector('#th').style.borderBottom = "3px solid transparent";
+  document.querySelector('#f').style.borderBottom = "3px solid transparent";
+  document.querySelector('#sa').style.borderBottom = "3px solid transparent";
 };
-
 document.getElementById("tu").onclick = function() {
   document.querySelector('#tu').style.color = "#276AAD";
   document.querySelector('#su').style.color = "black";
@@ -57,16 +220,14 @@ document.getElementById("tu").onclick = function() {
   document.querySelector('#th').style.color = "black";
   document.querySelector('#f').style.color = "black";
   document.querySelector('#sa').style.color = "black";
-  $('#tuesday').show();
-    $('#sunday').hide();
-    $('#monday').hide();
-    $('#wednesday').hide();
-    $('#thursday').hide();
-    $('#friday').hide();
-    $('#saturday').hide();
-    $('#top-five').hide();
+  document.querySelector('#tu').style.borderBottom = "3px solid #276AAD";
+  document.querySelector('#m').style.borderBottom = "3px solid transparent";
+  document.querySelector('#su').style.borderBottom = "3px solid transparent";
+  document.querySelector('#w').style.borderBottom = "3px solid transparent";
+  document.querySelector('#th').style.borderBottom = "3px solid transparent";
+  document.querySelector('#f').style.borderBottom = "3px solid transparent";
+  document.querySelector('#sa').style.borderBottom = "3px solid transparent";
 };
-
 document.getElementById("w").onclick = function() {
   document.querySelector('#w').style.color = "#276AAD";
   document.querySelector('#su').style.color = "black";
@@ -75,16 +236,14 @@ document.getElementById("w").onclick = function() {
   document.querySelector('#th').style.color = "black";
   document.querySelector('#f').style.color = "black";
   document.querySelector('#sa').style.color = "black";
-  $('#wednesday').show();
-    $('#sunday').hide();
-    $('#monday').hide();
-    $('#tuesday').hide();
-    $('#thursday').hide();
-    $('#friday').hide();
-    $('#saturday').hide();
-    $('#top-five').hide();
+  document.querySelector('#w').style.borderBottom = "3px solid #276AAD";
+  document.querySelector('#m').style.borderBottom = "3px solid transparent";
+  document.querySelector('#tu').style.borderBottom = "3px solid transparent";
+  document.querySelector('#su').style.borderBottom = "3px solid transparent";
+  document.querySelector('#th').style.borderBottom = "3px solid transparent";
+  document.querySelector('#f').style.borderBottom = "3px solid transparent";
+  document.querySelector('#sa').style.borderBottom = "3px solid transparent";
 };
-
 document.getElementById("th").onclick = function() {
   document.querySelector('#th').style.color = "#276AAD";
   document.querySelector('#su').style.color = "black";
@@ -93,16 +252,14 @@ document.getElementById("th").onclick = function() {
   document.querySelector('#m').style.color = "black";
   document.querySelector('#f').style.color = "black";
   document.querySelector('#sa').style.color = "black";
-  $('#thursday').show();
-    $('#sunday').hide();
-    $('#monday').hide();
-    $('#tuesday').hide();
-    $('#wednesday').hide();
-    $('#friday').hide();
-    $('#saturday').hide();
-    $('#top-five').hide();
+  document.querySelector('#th').style.borderBottom = "3px solid #276AAD";
+  document.querySelector('#m').style.borderBottom = "3px solid transparent";
+  document.querySelector('#tu').style.borderBottom = "3px solid transparent";
+  document.querySelector('#w').style.borderBottom = "3px solid transparent";
+  document.querySelector('#su').style.borderBottom = "3px solid transparent";
+  document.querySelector('#f').style.borderBottom = "3px solid transparent";
+  document.querySelector('#sa').style.borderBottom = "3px solid transparent";
 };
-
 document.getElementById("f").onclick = function() {
   document.querySelector('#f').style.color = "#276AAD";
   document.querySelector('#su').style.color = "black";
@@ -111,16 +268,14 @@ document.getElementById("f").onclick = function() {
   document.querySelector('#th').style.color = "black";
   document.querySelector('#m').style.color = "black";
   document.querySelector('#sa').style.color = "black";
-  $('#friday').show();
-    $('#sunday').hide();
-    $('#monday').hide();
-    $('#tuesday').hide();
-    $('#wednesday').hide();
-    $('#thursday').hide();
-    $('#saturday').hide();
-    $('#top-five').hide();
+  document.querySelector('#f').style.borderBottom = "3px solid #276AAD";
+  document.querySelector('#m').style.borderBottom = "3px solid transparent";
+  document.querySelector('#tu').style.borderBottom = "3px solid transparent";
+  document.querySelector('#w').style.borderBottom = "3px solid transparent";
+  document.querySelector('#th').style.borderBottom = "3px solid transparent";
+  document.querySelector('#su').style.borderBottom = "3px solid transparent";
+  document.querySelector('#sa').style.borderBottom = "3px solid transparent";
 };
-
 document.getElementById("sa").onclick = function() {
   document.querySelector('#sa').style.color = "#276AAD";
   document.querySelector('#su').style.color = "black";
@@ -129,55 +284,14 @@ document.getElementById("sa").onclick = function() {
   document.querySelector('#th').style.color = "black";
   document.querySelector('#f').style.color = "black";
   document.querySelector('#m').style.color = "black";
-  $('#saturday').show();
-    $('#sunday').hide();
-    $('#monday').hide();
-    $('#tuesday').hide();
-    $('#wednesday').hide();
-    $('#thursday').hide();
-    $('#friday').hide();
-    $('#top-five').hide();
+  document.querySelector('#sa').style.borderBottom = "3px solid #276AAD";
+  document.querySelector('#m').style.borderBottom = "3px solid transparent";
+  document.querySelector('#tu').style.borderBottom = "3px solid transparent";
+  document.querySelector('#w').style.borderBottom = "3px solid transparent";
+  document.querySelector('#th').style.borderBottom = "3px solid transparent";
+  document.querySelector('#f').style.borderBottom = "3px solid transparent";
+  document.querySelector('#su').style.borderBottom = "3px solid transparent";
 };
-
-// -------- Quick Find Button --------
-
-document.getElementById("quick-btn").onclick = function() {
-  document.querySelector('#quick-btn').style.visibility = "hidden";
-  document.querySelector('#selector').style.visibility = "hidden";
-  document.querySelector('#quick-btn').style.display = "none";
-  document.querySelector('#selector').style.display = "none";
-  document.querySelector('#back').style.visibility = "visible";
-  document.querySelector('#quick-info').style.visibility = "visible";
-  $('#top-five').show();
-    $('#sunday').hide();
-    $('#monday').hide();
-    $('#tuesday').hide();
-    $('#wednesday').hide();
-    $('#thursday').hide();
-    $('#friday').hide();
-    $('#saturday').hide();
-
-};
-
-  document.getElementById("back").onclick = function() {
-
-  document.querySelector('#quick-btn').style.visibility = "visible";
-  document.querySelector('#selector').style.visibility = "visible";
-
-  document.querySelector('#quick-btn').style.display = "flex";
-  document.querySelector('#selector').style.display = "flex";
-  document.querySelector('#back').style.visibility = "hidden";
-  document.querySelector('#quick-info').style.visibility = "hidden";
-  $('#sunday').show();
-  $('#monday').show();
-  $('#tuesday').show();
-  $('#wednesday').show();
-  $('#thursday').show();
-  $('#friday').show();
-  $('#saturday').show();
-  $('#top-five').show();
-};
-
 
 // ------- Favorite Button ---------
 // document.getElementById("fav-btn").onclick = function() {
